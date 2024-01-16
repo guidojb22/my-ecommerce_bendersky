7fe7fc8945b14112db5230b1f804bc189b1b1dfa
Guido <guidobendersky@hotmail.com>
2024-01-16 18:00:01 -0300

    Firebase1

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,23 +1,21 @@
 import { useEffect, useState } from "react"
-import obtenerProductos from "../Utilidades/data"
+import { doc, getDoc } from "firebase/firestore"
 import ItemDetail from "../ItemDetail/ItemDetail"
 import { useParams } from "react-router-dom"
+import db from "../../db/db"
 
 const ItemDetailContainer = () => {
   const [producto, setProducto] = useState({})
   const { id } = useParams()
 
   useEffect(()=>{
-    obtenerProductos
-      .then((respuesta)=> {
-        const productoEncontrado = respuesta.find( (prod)=> prod.id === id)
-        setProducto(productoEncontrado)
+    const productoRef = doc(db, "productos", id)
+    getDoc(productoRef)
+      .then ((respuesta)=> {
+        const productoDb = { id: respuesta.id, ...respuesta.data() }
+        setProducto(productoDb)
       })
-      .catch((err)=>{
-        console.log(err)
-      })
-
-  }, [])
+  }, [id])
 
   return (
     <div>
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,9 @@
 import "./ItemListContainer.css"
 import { useEffect, useState } from "react";
-
-import obtenerProductos from "../Utilidades/data";
 import ItemList from "../ItemList/ItemList";
 import { useParams } from "react-router-dom";
-
+import { collection, getDocs, query, where } from "firebase/firestore"
+import db from "../../db/db"
 
 const ItemListContainer = ({ saludo }) => {
     const [productos, setProductos] = useState([]);
@@ -12,23 +11,21 @@ const ItemListContainer = ({ saludo }) => {
     const { categoria } = useParams();
   
     useEffect(() => {
-      obtenerProductos
-        .then((respuesta) => {
-          if (categoria) {
-            const productosFiltrados = respuesta.filter(
-              (producto) => producto.categoria === categoria
-            );
-            setProductos(productosFiltrados);
-          } else {
-            setProductos(respuesta);
-          }
-        })
-        .catch((error) => {
-          console.log(error)
-        })
-        .finally(() => {
-          console.log("Finalizó la promesa.")
-        });
+      let consulta 
+      const productosRef = collection(db, "productos")
+      if (categoria) {
+        consulta = query(productosRef, where("categoria","==",categoria))
+      }else {
+        consulta = productosRef
+      }
+
+      getDocs(consulta)
+        .then((respuesta)=> {
+          let productosDb = respuesta.docs.map((producto)=>{
+            return { id: producto.id, ...producto.data() }
+          });
+          setProductos(productosDb)
+        }); 
     }, [categoria]);
   
     return(
